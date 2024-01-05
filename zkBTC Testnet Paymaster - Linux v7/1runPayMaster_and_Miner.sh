5f6435a81898813a70c3dba5de8863c006731e28
ZKBitcoinToken <128942687+ZKBitcoinToken@users.noreply.github.com>
2024-01-04 23:58:03 -0600

    Update 1runPayMaster_and_Miner.sh

--- zkBTC Testnet Paymaster - Linux v7/1runPayMaster_and_Miner.sh
+++ zkBTC Testnet Paymaster - Linux v7/1runPayMaster_and_Miner.sh
@@ -30,25 +30,28 @@ then
     exit 1
 fi
 
-
+#Check for dotnet
 if ! command -v dotnet &> /dev/null
 then
-    echo dotnet 5.0 is not found or not installed.
-    echo then run script again to install depenicies
+    echo "dotnet 5.0 is not found or not installed."
+    echo "Installing dotnet 5.0..."
 
+    # Add Microsoft package signing key and repository
+    wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
     sudo apt-get update; \
     sudo apt-get install -y apt-transport-https && \
     sudo apt-get update && \
     sudo apt-get install -y dotnet-sdk-5.0
 
     # Verify the installation
-
     dotnet --version
- 
-    echo dotnet 5.0 is installed rerun script to start
 
-    pause
+    echo "dotnet 5.0 is installed. Rerun the script to start."
+
     goto end
+else
+    echo "dotnet 5.0 is already installed."
 fi
 
 
